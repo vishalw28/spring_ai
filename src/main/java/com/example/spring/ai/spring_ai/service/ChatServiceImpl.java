d1f23ca61d139d9ae1769321e88e4f5494d3151f
Vishal Wagh <vishalw28@hotmail.com>
2026-03-27 16:34:05 +0530

    Fluent api usage such as metadata, generation result, llm response

--- src/main/java/com/example/spring/ai/spring_ai/service/ChatServiceImpl.java
+++ src/main/java/com/example/spring/ai/spring_ai/service/ChatServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.spring.ai.spring_ai.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,14 +19,27 @@ public class ChatServiceImpl implements ChatService{
 
     @Override
     public String chat(String query) {
-        String prompt = "Tell me about virat kohli";
+    
         // Prompt prompt = new Prompt(quer)
-        String content =chatClient.
-            prompt() // we can pass prompt here too.
-            .user(prompt)
-            .system("As an expert in cricket")
+        var content =chatClient.
+            prompt(new Prompt(query)) // we can pass prompt here too.
+            // .user(prompt)
+            // .system("As an expert in cricket")
             .call()
-            .content();
+            .chatResponse() // ChatResponse gives the metadata such as generation, assistent msg,
+            // .getMetadata(); 
+            /**
+                MetaDataResult: 
+                { id: chatcmpl-******, usage: DefaultUsage{promptTokens=8, completionTokens=9, totalTokens=17}, rateLimit: { @type: org.springframework.ai.openai.metadata.OpenAiRateLimit, requestsLimit: 10000, requestsRemaining: 9999, requestsReset: PT1M4S, tokensLimit: 200000; tokensRemaining: 199997; tokensReset: PT0S } }
+            
+            */
+           .getResult()
+           .getOutput()
+           .getText();
+
+
+        System.out.println(content);
+            //.content();
        return content;
     }
 
